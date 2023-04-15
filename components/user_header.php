d148bfbf091466ed96553e70cf4810aa6d2f6192
russcuevas <russelcuevas0@gmail.com>
2023-04-15 18:00:50 +0800

    third and fourth day

--- components/user_header.php
+++ components/user_header.php
@@ -1,10 +1,17 @@
 <?php 
-
+if(isset($message)){
+    foreach($message as $message){
+        echo '<div class="message">
+        <span>'.$message.'</span>
+        <i class="fas fa-times" onclick=".this.parentElement.remove();"></i>
+    </div>';
+    }
+}
 ?>
 
 <header class="header">
     <section class="flex">
-        <a href="home.php">FOOD ORDER SYSTEM</a>
+        <a href="home.php" class="logo">FOOD ORDER SYSTEM</a>
         <nav class="navbar">
             <a href="home.php">Home</a>
             <a href="about.php">About</a>
@@ -14,9 +21,15 @@
         </nav>
 
         <div class="icons">
+            <?php
+                $count_user_cart_items = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
+                $count_user_cart_items->execute(['user_id']);
+                $total_user_cart_items = $count_user_cart_items->rowCount();
+            ?>
         <a href="search.php"><i class="fas fa-search"></i></a>
-        <a href="cart.php"><i class="fas fa-shopping-cart"></i></a>
-        <div id="user-btn" class="fas fa-bars"></div>
+        <a href="cart.php"><i class="fas fa-shopping-cart"></i><span>(<?= $total_user_cart_items; ?>)</span></a>
+        <div id="user-btn" class="fas fa-user"></div>
+        <div id="menu-btn" class="fas fa-bars"></div>
         </div>
     </section>
 </header>
